7373d7973090dec6b3a0c5e3ec149d7db14f5f02
rzhancock <rzhancock@gmail.com>
2018-08-26 18:49:01 -0700

    working prototype without transitions

--- src/QuoteApp.js
+++ src/QuoteApp.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './quotes.css';
 import { favoriteQuotes } from './quotes';
+import ReactCSSTransitionReplace from 'react-css-transition-replace';
 
 
 export default class QuoteApp extends Component {
     constructor(props) {
         super(props);
+
+        
+        const startingIndex = Math.round(Math.random() * 30);
+        
 
         this.state = {
             quotes: favoriteQuotes,
@@ -13,12 +18,12 @@ export default class QuoteApp extends Component {
             quoteIndexes: [],
             authors: [],
             images: {},
-            imageIndex: Math.round(Math.random() * 30),
-            imageIndex2: Math.round(Math.random() * 30 - 1),
+            imageIndex: startingIndex,
             imageIndexes:[],
             URL: '',
-            URL2: '',
-            apiImagePage: 'page=1'
+            apiImagePage: 'page=1',
+            isLoading: false,
+            hasErrored: false
         }
         
     }
@@ -28,12 +33,31 @@ export default class QuoteApp extends Component {
         const { quoteIndex, quotes } = this.state;
         
         return quotes[quoteIndex].text;
-    }
+    };
 
     renderAuthor = () => {
         const { quoteIndex, quotes } = this.state;
                
         return quotes[quoteIndex].author;
+    }
+
+    fetchData(url) {
+        this.setState({ isLoading: true });
+
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+
+                this.setState({ isLoading: false });
+
+                return response;
+            })
+            .then((response) => response.json())
+            .then((images) => this.setState({ images }))
+            .catch(() => this.setState({ hasErrored: true }))
+            .then(this.updateURL);
     }
 
     updateURL = () => {
@@ -42,19 +66,17 @@ export default class QuoteApp extends Component {
             quoteIndex, 
             quoteIndexes, 
             images, 
-            imageIndex, 
-            imageIndex2, 
+            imageIndex,
             imageIndexes, 
             authors } = this.state;
 
         const pushQuote = [...quoteIndexes, quoteIndex];
-        const pushImages = [...imageIndexes, imageIndex, imageIndex2];
+        const pushImages = [...imageIndexes, imageIndex];
         const pushAuthors = [...authors, quotes[quoteIndex].author];
 
              this.setState(
                     { 
                         URL: images[imageIndex].urls.regular,
-                        URL2: images[imageIndex2].urls.regular,
                         quoteIndexes: pushQuote,
                         imageIndexes: pushImages,
                         authors: pushAuthors
@@ -64,24 +86,12 @@ export default class QuoteApp extends Component {
     
 
     componentDidMount(){
-
-        let xhr = new XMLHttpRequest();
-
-        xhr.open('GET', `https://api.unsplash.com/collections/2157113/photos?fit=crop&w=900&h=600&
+        this.fetchData(`https://api.unsplash.com/collections/2157113/photos?fit=crop&w=900&h=600&
             ${this.state.apiImagePage}
             &per_page=30&client_id=d78aa27606ff8868b76ac8d0cb6f4ea3c4010b12735789c34ee4bb0f98b4e132`);
 
-        xhr.onload = () => {
-            this.setState({
-                images: JSON.parse(xhr.responseText),
-            }, this.updateURL);
-        }
-
-        xhr.send();
+        
     }
-
-    
-
 
 
     nextIndex = () => {
@@ -91,28 +101,22 @@ export default class QuoteApp extends Component {
         const numberOfImages = images.length - 1;
         const newIndex = Math.round(Math.random() * numberOfQuotes);
         const newImage = Math.round(Math.random() * numberOfImages);
-        const newImage2 = newImage - 1;
 
-        if (quoteIndexes.indexOf(newIndex) !== -1 || imageIndexes.indexOf(newImage) !== -1 || imageIndexes.indexOf(newImage2) !== -1) {
+
+
+        if (quoteIndexes.indexOf(newIndex) !== -1 || imageIndexes.indexOf(newImage) !== -1) {
             return this.nextIndex();
         }
 
-        this.setState({
-            quoteIndex: newIndex,
-            imageIndex: newImage,
-            imageIndex2: newImage2
-        }, this.updateURL);
 
-}
+          this.setState({
+                quoteIndex: newIndex,
+                imageIndex: newImage,
+            }, this.updateURL);  
 
-
-
+    }
 
     render() {
-     
-      /* const styles = {
-                transition: 'opacity 1s ease-in'
-        };   */ 
          
 
         return (
@@ -141,16 +145,15 @@ export default class QuoteApp extends Component {
                 <div className="bottom-left-corner"></div>
 
                 <div className="quote-container">
-                    <div  className="background">
-                        <img src={this.state.URL} alt="background" className="image1"/>
-                        <img src={this.state.URL2} alt="background image2" className="image2"/>
-                    </div>
-                    <div className="quote">
+                     <div id="quote" >
                         {this.renderQuote()}
                     </div>
 
-                    <div className="author">
+                    <div id="author" >
                         {this.renderAuthor()}
+                    </div>
+                    <div className="background">
+                            <img src={this.state.URL} key={this.state.imageIndex} alt="" id="image1" />
                     </div>
                 </div>
 
